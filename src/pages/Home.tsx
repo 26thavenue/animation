e8c144f6aedc8032f5656f558995fd08db3f0dbb
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2025-07-09 20:39:33 +0100

    Making a change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,6 +28,12 @@ const Home = () => {
                     <h3 className="max-w-xs">Glow Button</h3>
                 </div>
             </Link>
+
+            <Link to="/day4">
+                <div className="w-[300px] h-[200px] bg-amber-100 flex items-center justify-center px-3">
+                    <h3 className="max-w-xs">Parallax</h3>
+                </div>
+            </Link>
             
         </div>
     </div>
